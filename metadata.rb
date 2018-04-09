1fc20327898e77f4432748acfd974979e22550e8
Vickie <9098023+PhishyFish@users.noreply.github.com>
2018-04-09 15:47:32 -0700

    Add Berksfile and metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name        "hubbub"
+description "Installs Hubbub"
+maintainer  "Vickie Chen"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'apt', '= 6.1.4'
+depends 'ruby-ng'
+depends 'nodejs'
+depends 'postgres'
+depends 'nginx'
+depends 'unicorn'
