d84403ff891cf09c6f352c4c0b27f12b69ecaf2c
Vickie <9098023+PhishyFish@users.noreply.github.com>
2018-04-09 15:55:37 -0700

    Add recipes, default attributes; move files into hubbub folder

---
+++ hubbub/attributes/default.rb
@@ -0,0 +1,20 @@
+default['ruby-ng']['ruby_version'] = "2.4"
+default['nodejs']['version'] = "6.10.1"
+default['nodejs']['npm']['version'] = "5.2.0"
+default['nginx']['default_site_enabled'] = false
+
+default[:hubbub][:git_repository] = "https://github.com/PhishyFish/hubbub/"
+default[:hubbub][:git_revision] = "master"
+default[:hubbub][:path] = "/opt/hubbub"
+
+default[:hubbub][:rails_env] = "production"
+default[:hubbub][:log_to_stdout] = "true"
+
+default[:hubbub][:environment] = {
+  "SECRET_KEY_BASE": node[:hubbub][:secret_key_base],
+  "DATABASE_URL": node[:hubbub][:database_url],
+  "RAILS_ENV": node[:hubbub][:rails_env],
+  "RAILS_LOG_TO_STDOUT": node[:hubbub][:log_to_stdout]
+}
+
+default[:hubbub][:start_cmd] = "unicorn -E production -c /opt/unicorn.rb"
--- hubbub/recipes/deploy.rb
+++ hubbub/recipes/deploy.rb
@@ -1,1 +1,63 @@
+service "hubbub" do
+  action [:stop]
+end
+
+directory "#{node[:hubbub][:path]}" do
+  owner 'ubuntu'
+  group 'ubuntu'
+  mode '0755'
+  recursive true
+  action :create
+end
+
 # deploy script here
+git "#{node[:hubbub][:path]}" do
+  repository node[:hubbub][:git_repository]
+  revision node[:hubbub][:git_revision]
+  environment ({"HOME"=>"/home/ubuntu"})
+  action :sync
+  user "ubuntu"
+end
+
+execute "Install Gems" do
+  cwd node[:hubbub][:path]
+  command "bundle install"
+  user "ubuntu"
+  # group new_resource.group
+  environment ({"HOME"=>"/home/ubuntu"})
+  # not_if { package_installed? }
+end
+
+execute "Install NPM packages" do
+  cwd node[:hubbub][:path]
+  command "npm install"
+  user "ubuntu"
+  # group new_resource.group
+  environment ({"HOME"=>"/home/ubuntu"})
+  # not_if { package_installed? }
+end
+
+execute "Compile Webpack Assets" do
+  cwd node[:hubbub][:path]
+  command "./node_modules/.bin/webpack"
+  environment ({"NODE_ENV": "production", "HOME": "/home/ubuntu"})
+  user "ubuntu"
+end
+
+execute "Clobber Rails Assets" do
+  cwd node[:hubbub][:path]
+  command "bundle exec rake assets:clobber"
+  environment ({"RAILS_ENV": "production", "HOME": "/home/ubuntu"})
+  user "ubuntu"
+end
+
+execute "Compile Rails Assets" do
+  cwd node[:hubbub][:path]
+  command "bundle exec rake assets:precompile"
+  environment ({"RAILS_ENV": "production", "HOME": "/home/ubuntu"})
+  user "ubuntu"
+end
+
+service "hubbub" do
+  action [ :enable, :start ]
+end
